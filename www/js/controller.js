f9e8f9a26bfaa26b71f25bd080b760e6a6a71cb1
Aaron Brown <aaron.kaoru.brown@gmail.com>
2015-02-24 00:32:25 -0800

    ng-click events working

--- www/js/controller.js
+++ www/js/controller.js
@@ -6,8 +6,35 @@ angular.module('crashrApp.controllers', []).controller('eventsController', funct
     for(var i=0; i < e.events.length; i++){
       $scope.events[i] = e.events[i];
     }
-    //alert($scope.events[2].eventTitle);
+    $scope.nextEvent = function(){
+      var nEvents = $scope.events.length;
+      var nCounter = Math.floor(Math.random() * nEvents);
+      var oEvent = $scope.events[nCounter];
+      $scope.currentEvent = oEvent;
+      //console.log($scope.currentEvent.eventTitle);
+    }
+
+
+
+    $scope.nextEvent();
+
+
   }).error(function(e){
     console.log("No, no, no, no, no, no, NOOOO. FRICK"); // Error message thing
   });
+
+  $scope.noEvent = function(){
+    var string = "Decline";
+    $scope.nextEvent();
+  }
+
+  $scope.yesEvent = function(){
+    var string = "RSVP!";
+    $scope.nextEvent();
+  }
+
+  $scope.infoEvent = function(){
+    var string = "EVENT INFO";
+  }
+
 });
